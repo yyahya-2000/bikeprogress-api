3737c1f7e126575eed297da10ff1992762e2570f
Yanal Yahya <yanal.alyhya@gmail.com>
2022-07-20 12:59:29 +0300

    add purchases, contacts and certificates routes to api.php - RAZRABOTKA-980

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CertificateController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\PurchaseController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -28,6 +31,26 @@ Route::middleware(['auth:sanctum', 'admin'])->group(function () {
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', [AuthController::class, 'user']);
     Route::post('logout', [AuthController::class, 'logout']);
+
+    Route::get('contacts', [ContactController::class, 'index']);
+    Route::get('contact/read/{id}', [ContactController::class, 'show']);
+    Route::post('contact/edit', [ContactController::class, 'update']);
+    Route::post('contact/delete', [ContactController::class, 'destroy']);
+    Route::post('contact/add', [ContactController::class, 'create']);
+
+    Route::get('certificates', [CertificateController::class, 'index']);
+    Route::get('certificate/read/{id}', [CertificateController::class, 'show']);
+    Route::post('certificate/edit', [CertificateController::class, 'update']);
+    Route::post('certificate/delete', [CertificateController::class, 'destroy']);
+    Route::post('certificate/add', [CertificateController::class, 'add']);
+
+    Route::get('purchases', [PurchaseController::class, 'index']);
+    Route::get('purchase/read/{id}', [PurchaseController::class, 'show']);
+    Route::post('purchase/edit', [PurchaseController::class, 'update']);
+    Route::post('purchase/delete', [PurchaseController::class, 'destroy']);
+    Route::post('purchase/add', [PurchaseController::class, 'add']);
+
+
 });
 
 Route::controller(AuthController::class)->group(function () {
